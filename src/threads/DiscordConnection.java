430aea7a35731289c1b7b099ef5b637a99fc3561
Shai Cohen <shaico90@gmail.com>
2016-10-05 04:33:15 +0300

    v1.4.0

--- src/threads/DiscordConnection.java
+++ src/threads/DiscordConnection.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import application.MyController;
 import de.btobastian.javacord.DiscordAPI;
 import de.btobastian.javacord.Javacord;
